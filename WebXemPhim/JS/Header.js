7c8d604951827f91447cf76c4255d44ff9e00e0f
Xuyen <hoangxuyenpham30@gmail.com>
2023-04-17 21:02:50 +0700

    x

--- WebXemPhim/JS/Header.js
+++ WebXemPhim/JS/Header.js
@@ -1,28 +1,27 @@
 import { allMovies } from "../DATA/DataALLMovies.js";
 
-document.addEventListener("DOMContentLoaded", () => {
-   const searchBox = document.querySelector("#search-box");
-   const searchInput = document.querySelector("#search-text");
-   const returnSearch = document.querySelector("#return-search");
+const searchBox = document.querySelector("#search-box");
+const searchInput = document.querySelector("#search-text");
+const returnSearch = document.querySelector("#return-search");
 
-   searchBox.addEventListener("input", () => {
-      const searchTerm = searchInput.value.toLowerCase();
-      const searchResults = allMovies.filter((movie) => {
-         for (let prop in movie) {
-            if (movie[prop].toString().toLowerCase().includes(searchTerm)) {
-               return true;
-            }
+searchBox.addEventListener("input", () => {
+   const searchTerm = searchInput.value.toLowerCase();
+   const searchResults = allMovies.filter((movie) => {
+      for (let prop in movie) {
+         if (movie[prop].toString().toLowerCase().includes(searchTerm)) {
+            return true;
          }
-         return false;
-      });
+      }
+      return false;
+   });
 
-      if (searchTerm === "") {
-         returnSearch.innerHTML = "";
-         returnSearch.style.display = "none";
-      } else if (searchResults.length > 0) {
-         const searchItems = searchResults
-            .map((result) => {
-               return `
+   if (searchTerm === "") {
+      returnSearch.innerHTML = "";
+      returnSearch.style.display = "none";
+   } else if (searchResults.length > 0) {
+      const searchItems = searchResults
+         .map((result) => {
+            return `
         <li class="menu-item">
           <a href="${result.link1}" title="${result.name}">
             <img src="${result.image}" alt="${result.name}" />
@@ -33,13 +32,12 @@ document.addEventListener("DOMContentLoaded", () => {
           </a>
         </li>
       `;
-            })
-            .join("");
-         returnSearch.innerHTML = `<ul id="return-card">${searchItems}</ul>`;
-         returnSearch.style.display = "block";
-      } else {
-         returnSearch.innerHTML = `<p>No results found.</p>`;
-         returnSearch.style.display = "block";
-      }
-   });
+         })
+         .join("");
+      returnSearch.innerHTML = `<ul id="return-card">${searchItems}</ul>`;
+      returnSearch.style.display = "block";
+   } else {
+      returnSearch.innerHTML = `<p>No results found.</p>`;
+      returnSearch.style.display = "block";
+   }
 });
