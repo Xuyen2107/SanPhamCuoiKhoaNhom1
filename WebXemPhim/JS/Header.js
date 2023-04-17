47ef60ec5c7141815b167f6bd338a4e24b36250a
Xuyen <hoangxuyenpham30@gmail.com>
2023-04-17 19:48:28 +0700

    xuyen

--- WebXemPhim/JS/Header.js
+++ WebXemPhim/JS/Header.js
@@ -1,42 +1,45 @@
 import { allMovies } from "../DATA/DataALLMovies.js";
 
-const searchBox = document.querySelector("#search-box");
-const searchInput = document.querySelector("#search-text");
-const returnSearch = document.querySelector("#return-search");
+document.addEventListener("DOMContentLoaded", () => {
+   const searchBox = document.querySelector("#search-box");
+   const searchInput = document.querySelector("#search-text");
+   const returnSearch = document.querySelector("#return-search");
 
-searchBox.addEventListener("input", () => {
-   const searchTerm = searchInput.value.toLowerCase();
-   const searchResults = allMovies.filter((movie) => {
-      for (let prop in movie) {
-         if (movie[prop].toString().toLowerCase().includes(searchTerm)) {
-            return true;
+   searchBox.addEventListener("input", () => {
+      const searchTerm = searchInput.value.toLowerCase();
+      const searchResults = allMovies.filter((movie) => {
+         for (let prop in movie) {
+            if (movie[prop].toString().toLowerCase().includes(searchTerm)) {
+               return true;
+            }
          }
-      }
-      return false;
-   });
+         return false;
+      });
 
-   if (searchTerm === "") {
-      returnSearch.innerHTML = "";
-      returnSearch.style.display = "none";
-   } else if (searchResults.length > 0) {
-      const searchItems = searchResults
-         .map((result) => {
-            return `
+      if (searchTerm === "") {
+         returnSearch.innerHTML = "";
+         returnSearch.style.display = "none";
+      } else if (searchResults.length > 0) {
+         const searchItems = searchResults
+            .map((result) => {
+               return `
         <li class="menu-item">
-          <a href="${result.link1}">
+          <a href="${result.link1}" title="${result.name}">
             <img src="${result.image}" alt="${result.name}" />
             <div>
               <p>${result.name}</p>
+              <p>${result.year}</p>
             </div>
           </a>
         </li>
       `;
-         })
-         .join("");
-      returnSearch.innerHTML = `<ul id="return-card">${searchItems}</ul>`;
-      returnSearch.style.display = "block";
-   } else {
-      returnSearch.innerHTML = `<p>No results found.</p>`;
-      returnSearch.style.display = "block";
-   }
+            })
+            .join("");
+         returnSearch.innerHTML = `<ul id="return-card">${searchItems}</ul>`;
+         returnSearch.style.display = "block";
+      } else {
+         returnSearch.innerHTML = `<p>No results found.</p>`;
+         returnSearch.style.display = "block";
+      }
+   });
 });
